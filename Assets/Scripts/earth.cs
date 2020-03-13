ded203a8be6b8ba722dd19d8049cd54cab625386
neslihan <cetinkayaneslihan98@gmail.com>
2020-03-13 22:42:26 +0300

    Test

---
+++ Assets/Scripts/earth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earth : MonoBehaviour
+{
+    public sun s;
+    public float rotationSpeed = 0;
+    public float orbitSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * rotationSpeed, 0, 0);
+        transform.RotateAround(s.transform.position, Vector3.up, Time.deltaTime * orbitSpeed);
+    }
+}
---
+++ Assets/Scripts/venus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class venus : MonoBehaviour
+{
+    public sun s;
+    public float rotationSpeed = 0;
+    public float orbitSpeed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // v.transform.Rotate(0, Time.deltaTime * rotationSpeed, 0);
+    }
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * rotationSpeed, 0, 0);
+        transform.RotateAround(s.transform.position, Vector3.up, Time.deltaTime * orbitSpeed);
+    }
+}
